69ef481ffc83c875577c71e2ffcdf0f4b415a14c
Steph <stephbarron95@gmail.com>
2021-09-08 11:31:07 -0400

    Built app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Dictionary from "./dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+         
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
+    </div>
+  );
+}
